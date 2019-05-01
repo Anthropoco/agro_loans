c454dcf29116fa07ba22bd3ca5d93b1a4c4d8b81
Anthropoco <aeneh90@gmail.com>
2019-05-01 17:01:32 +0100

    Do index.php html template

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="styles.css"	 />
+		<title>Agro Loans</title>
+
+	</head>
+	<body>
+		<header>
+			<div class="site_logo">
+			</div>
+			<h1 class="site_title">Agro Loans</h1>
+			<p class="site_tagline">help at your finger tips</p>
+		</header>
+		<section class="content">
+			<p>we are an agro extension firm. We are approved with the Bank of Agriculture to give low interest loans and other aids to farmers.</p>
+			<h2>need a <span class="emphasis">loan?</span></h2>
+			<button class="btn btn-large">apply now</button>
+		</section>
+		<footer>(c) Pocosoft technologies 2019</footer>
+	</body>
+</html>
